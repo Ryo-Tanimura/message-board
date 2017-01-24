04ca995859296d183a059c144366615e13d69857
RyoTanimura <zeromegalpha@gmail.com>
2017-01-24 10:29:16 +0000

    年齢追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116160145) do
+ActiveRecord::Schema.define(version: 20170124101510) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
